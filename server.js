2a96d43a1ead549e35cff9ce065dda60338afef1
Lee Rouse <leerouse@Lees-MacBook-Pro.local>
2017-09-21 13:46:23 -0600

    added html and pizza menu

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var app = express();
-var port = 6000;
+var port = 5000;
+
+app.get('/', function(req, res){
+  res.sendfile("./templates/pizza.html");
+});
+
+
+
 app.listen(port, function(){
   console.log("listening at " + port);
 });
+
